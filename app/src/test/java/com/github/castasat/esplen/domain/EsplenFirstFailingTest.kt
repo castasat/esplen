9daddd577512e6375ad142ce57ca5b6fdf4bc8d0
Denis Dmitrienko <castasat@mail.ru>
2021-02-02 02:07:38 +0300

    started first TDD cycle by adding EsplenFirstFailingTest

---
+++ app/src/test/java/com/github/castasat/esplen/domain/EsplenFirstFailingTest.kt
@@ -0,0 +1,11 @@
+package com.github.castasat.esplen.domain
+
+import org.junit.Assert.fail
+import org.junit.Test
+
+class EsplenFirstFailingTest {
+    @Test
+    fun domainIsPresent() {
+        fail("Domain is not present yet, you know?")
+    }
+}
